e7115c52a8ba8d0f6275f684b343580a78f8b1a1
pphein <pph.hein87@gmail.com>
2023-06-16 07:44:54 +0630

    Refactored

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var socket = require('socket.io');
 
 // App setup
 var app = express();
-var server = app.listen(8000, function() {
+var server = app.listen(8000, function () {
     console.log('listening for requests on port 8000, http://localhost:8000');
 });
 
@@ -15,8 +15,16 @@ var io = socket(server);
 io.on('connection', (socket) => {
     console.log("Someone connected id", socket.id);
 
-    socket.on('restart', function() {
+    socket.on('restart', function () {
         io.sockets.emit('restart');
+    });
+
+    socket.on('increase', function () {
+        io.sockets.emit('increase');
+    });
+
+    socket.on('decrease', function (data) {
+        io.sockets.emit('decrease', data);
     });
 
 })
--- public/app.js
+++ public/app.js
@@ -1,14 +1,39 @@
 var socket = io.connect('http://localhost:8000');
 
+var value = 0;
+
 var restart = document.getElementById('restart');
+var increase = document.getElementById('increase');
+var decrease = document.getElementById('decrease');
 
+increase.addEventListener('click', function () {
+    value = value + 1;
+    socket.emit('increase', value);
+});
 
-restart.addEventListener('click', function() {
+decrease.addEventListener('click', function () {
+    value = value - 1;
+    socket.emit('decrease', value);
+});
+
+restart.addEventListener('click', function () {
     socket.emit('restart');
 });
 
+socket.on('increase', function (data) {
+    value = data
+    document.getElementById('value').innerHTML = value;
+    // alert(value);
+});
 
-socket.on('restart', function() {
+socket.on('decrease', function (data) {
+    value = data;
+    document.getElementById('value').innerHTML = value;
+    // alert(data);
+});
+
+
+socket.on('restart', function () {
     alert('Restart');
     window.location.reload(true);
 });
